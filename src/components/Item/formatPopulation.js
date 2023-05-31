e6be5104a1305304996ded45f61bf53f2bff73fe
julien <dev.guilbaud@gmail.com>
2023-05-31 15:05:18 +0200

    remplace . par ,

--- src/components/Item/formatPopulation.js
+++ src/components/Item/formatPopulation.js
@@ -5,11 +5,11 @@ export function formatpopulation(nombre) {
     }
     if (nombre<1000000){
       result = (nombre/1000).toString()
-      return result.slice(0,3)+" K"
+      return result.slice(0,3).replace(".",",")+" K"
     }
     if (nombre>999999){
         result = (nombre/1000000).toString()
-        return result.slice(0,3)+" M"
+        return result.slice(0,3).replace(".",",")+" M"
       }
 
 
